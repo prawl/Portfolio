df8161a03d1832c3d471f468d592abddd294688c
prawl <prawlings@assetricity.com>
2013-10-15 20:22:39 -0400

    Inital Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Portfolio::Application.config.secret_token = 'c87530c52a5158cce7e7f33de0ecb1f550d1920ae030a854d8a9cd7eca7bae0160f9c771874eafbf3a1e70160e752311966c83423d3c0c60403851bb4367a4fb'
