cf47dc9f8c91eb2a4b01f8e6a75585a890370590
Patrick Rawlings <ptyrawlings@gmail.com>
2014-07-24 21:52:01 -0400

    Upgraded to rails 4

--- config/application.rb
+++ config/application.rb
@@ -9,8 +9,10 @@ if defined?(Bundler)
   # Bundler.require(:default, :assets, Rails.env)
 end
 
+
 module Portfolio
   class Application < Rails::Application
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
@@ -51,7 +53,6 @@ module Portfolio
     # This will create an empty whitelist of attributes available for mass-assignment for all models
     # in your app. As such, your models will need to explicitly whitelist or blacklist accessible
     # parameters by using an attr_accessible or attr_protected declaration.
-    config.active_record.whitelist_attributes = true
 
     # Enable the asset pipeline
     config.assets.enabled = true
