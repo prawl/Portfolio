f69a32a54f3c8b88de7de18d2f8ac68511523b75
Patrick Rawlings <ptyrawlings@gmail.com>
2015-04-06 22:21:31 -0400

    Don't need test dir

--- test/functional/treasure_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class TreasureControllerTest < ActionController::TestCase
-  test "should get index" do
-    get :index
-    assert_response :success
-  end
-
-end
--- test/unit/helpers/assetricity_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class AssetricityHelperTest < ActionView::TestCase
-end
