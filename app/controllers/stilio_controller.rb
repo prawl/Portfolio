0d598141588b6be114505fc6d7622b3f1bb623e3
Patrick Rawlings <ptyRawlings@gmail.com>
2017-10-16 16:23:29 -0400

    Added Stilio

---
+++ app/controllers/stilio_controller.rb
@@ -0,0 +1,2 @@
+class StilioController < ApplicationController
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,15 @@
 Portfolio::Application.routes.draw do
   root to: 'welcome#index'
-  resources :zozi, only: [:index]
   resources :assetricity, only: [:index]
   resources :contact, only: [:index]
   resources :gbm, only: [:index]
   resources :jenga, only: [:index]
-  resources :treasure, only: [:index]
   resources :prepped, only: [:index]
   resources :resume, only: [:index]
+  resources :stilio, only: [:index]
+  resources :treasure, only: [:index]
+  resources :uncqi, only: [:index]
   resources :weaveup, only: [:index]
   resources :welcome, only: [:index]
-  resources :uncqi, only: [:index]
+  resources :zozi, only: [:index]
 end
